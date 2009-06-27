868807dddaa2995536ca2ebabf97ccef58e994ae
Frederic Bregier <nomail@nomail.com>
2009-06-27 16:03:48 +0000

    fix typo
    git-svn-id: http://goldengate.svn.sourceforge.net/svnroot/goldengate/GoldenGateFtp/trunk@145 7864f6d1-e753-4b29-85b9-74ae93846b71

--- src/main/java/goldengate/ftp/core/command/FtpCommandCode.java
+++ src/main/java/goldengate/ftp/core/command/FtpCommandCode.java
@@ -1,22 +1,22 @@
 /**
- * Copyright 2009, Frederic Bregier, and individual contributors
- * by the @author tags. See the COPYRIGHT.txt in the distribution for a
- * full listing of individual contributors.
+ * Copyright 2009, Frederic Bregier, and individual contributors by the @author
+ * tags. See the COPYRIGHT.txt in the distribution for a full listing of
+ * individual contributors.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 3.0 of
- * the License, or (at your option) any later version.
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3.0 of the License, or (at your option)
+ * any later version.
  *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
  */
 package goldengate.ftp.core.command;
 
@@ -247,7 +247,6 @@ public enum FtpCommandCode {
      *                E - EBCDIC|    | C - Carriage Control (ASA)
      *                          /    \
      *                I - Image
-     *
      *                L &lt;byte size&gt; - Local byte Byte size
      * </pre>
      *
@@ -1169,7 +1168,8 @@ public enum FtpCommandCode {
      *         MLST, ...)
      */
     public static boolean isListLikeCommand(FtpCommandCode command) {
-        return command == LIST || command == NLST || command == MLSD || command == MLST;
+        return command == LIST || command == NLST || command == MLSD ||
+                command == MLST;
     }
 
     /**
@@ -1180,7 +1180,8 @@ public enum FtpCommandCode {
      *         STAT, ...)
      */
     public static boolean isSpecialCommand(FtpCommandCode command) {
-        return command == QUIT || command == ABOR || command == NOOP || command == STAT;
+        return command == QUIT || command == ABOR || command == NOOP ||
+                command == STAT;
     }
 
     /**
