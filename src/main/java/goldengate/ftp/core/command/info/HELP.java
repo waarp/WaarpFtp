b6683fdaf6eaa4bfe9cb9e7d1d1ce1eb973cd391
Frederic Bregier <nomail@nomail.com>
2010-12-11 19:57:45 +0000

    Fix licenses
    git-svn-id: http://goldengate.svn.sourceforge.net/svnroot/goldengate/GoldenGateFtp/trunk@677 7864f6d1-e753-4b29-85b9-74ae93846b71

--- src/main/java/goldengate/ftp/core/command/info/HELP.java
+++ src/main/java/goldengate/ftp/core/command/info/HELP.java
@@ -1,22 +1,22 @@
 /**
- * Copyright 2009, Frederic Bregier, and individual contributors by the @author
- * tags. See the COPYRIGHT.txt in the distribution for a full listing of
- * individual contributors.
- *
- * This is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 3.0 of the License, or (at your option)
- * any later version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this software; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
- * site: http://www.fsf.org.
+   This file is part of GoldenGate Project (named also GoldenGate or GG).
+
+   Copyright 2009, Frederic Bregier, and individual contributors by the @author
+   tags. See the COPYRIGHT.txt in the distribution for a full listing of
+   individual contributors.
+
+   All GoldenGate Project is free software: you can redistribute it and/or 
+   modify it under the terms of the GNU General Public License as published 
+   by the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   GoldenGate is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with GoldenGate .  If not, see <http://www.gnu.org/licenses/>.
  */
 package goldengate.ftp.core.command.info;
 
